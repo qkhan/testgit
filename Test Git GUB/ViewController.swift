95e38706b5e56076da12cc2dc5d07a310aa0ac31
Qaisar Khan <qaisar@Qaisars-Mac-mini.local>
2017-05-27 21:42:22 +1000

    Initial Commit

---
+++ Test Git GUB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Git GUB
+//
+//  Created by Qaisar Khan on 27/5/17.
+//  Copyright © 2017 Qaisar Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
